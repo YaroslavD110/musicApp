df162c9de494935fadd0b3af76ed1e044f66fd71
tangerine69 <tangerine69@gmail.com>
2018-08-27 22:02:08 +0300

    fixed index paths

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 import { routerMiddleware as createRouterMiddleware } from "react-router-redux";
 import history from "./history";
@@ -10,7 +11,7 @@ const routerMiddleware = createRouterMiddleware(history);
 
 export default createStore(
   rootReducer,
-  applyMiddleware(sagaMiddleware, routerMiddleware)
+  applyMiddleware(logger, sagaMiddleware, routerMiddleware)
 );
 
 sagaMiddleware.run(rootSaga);
